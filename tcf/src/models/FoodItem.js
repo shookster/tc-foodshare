a08dd8c0bd207fcd8712d015cc01988a4fffd965
Ethan Shook <eshook89@gmail.com>
2020-12-07 15:36:58 -0600

    added models folder schema and javascript

---
+++ tcf/src/models/FoodItem.js
@@ -0,0 +1,20 @@
+module.exports = function (sequelize, DataTypes) {
+  var FoodItem = sequelize.define("food_item", {
+    status: DataTypes.STRING(50),
+    item_category: DataTypes.STRING(50),
+    item_description: DataTypes.STRING(50),
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+  FoodItem.associate = function (models) {
+    FoodItem.belongsTo(models.Location, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
+
+  return FoodItem;
+};
