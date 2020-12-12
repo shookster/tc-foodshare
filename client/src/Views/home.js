c1910c16c767a2af2e416574172ba4b52856f287
Zekarias Negassa <zakfena@gmail.com>
2020-12-12 02:04:45 -0600

    added input cardF

--- client/src/Views/home.js
+++ client/src/Views/home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import './home.css'
+// import './home.css';
 
 function Home(){
     return (
