0d88d6235f77678a616b4ac698cdd7389ca95fb3
Ruth Vang <r.vang012@gmail.com>
2020-12-11 19:50:47 -0600

    addingimage

--- client/src/Views/home.js
+++ client/src/Views/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import './home.css'
 
 function Home(){
     return (
-        <div>
+        <div className="backgroudImage">
+    
         <Navbar />
         <Footer />
         </div>
