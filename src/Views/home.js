67ac5082a8e271291bd6d42de599357bab918359
will collins <willdoinc@gmail.com>
2020-12-11 19:37:36 -0600

    new

--- src/Views/home.js
+++ src/Views/home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from '../components/navbar'
-import Footer from '../components/footer';
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer';
 
 function Home(){
     return (
