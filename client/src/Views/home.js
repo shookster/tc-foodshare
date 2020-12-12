ece6a49d55505bcd816bc76eeddcc4f331bdc3e0
Zekarias Negassa <zakfena@gmail.com>
2020-12-11 20:07:16 -0600

    added background image

--- client/src/Views/home.js
+++ client/src/Views/home.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import './home.css'
+// import './home.css';
+import background from '../components/image/food.jpg'
 
 function Home(){
     return (
-        <div className="backgroudImage">
+        <div className="backgroudImage" style ={{backgroundImage:`url(${background})`}}>
     
         <Navbar />
+        <p>
+            lksjdlkfjslkjdflskjdflksjdflkjslkljlkjk <br/>
+            ksdjflkjsdlkfjsldkfjlskdjfklsjdflkjsdlkj <br/>
+            lksjdlkfjslkjdflskjdflksjdflkjslkljlkjk <br/>
+            ksdjflkjsdlkfjsldkfjlskdjfklsjdflkjsdlkj <br/>
+            lksjdlkfjslkjdflskjdflksjdflkjslkljlkjk <br/>
+            ksdjflkjsdlkfjsldkfjlskdjfklsjdflkjsdlkj <br/>
+            lksjdlkfjslkjdflskjdflksjdflkjslkljlkjk <br/>
+            ksdjflkjsdlkfjsldkfjlskdjfklsjdflkjsdlkj <br/>
+            lksjdlkfjslkjdflskjdflksjdflkjslkljlkjk <br/>
+            ksdjflkjsdlkfjsldkfjlskdjfklsjdflkjsdlkj <br/>
+            lksjdlkfjslkjdflskjdflksjdflkjslkljlkjk <br/>
+            ksdjflkjsdlkfjsldkfjlskdjfklsjdflkjsdlkj <br/>
+            lksjdlkfjslkjdflskjdflksjdflkjslkljlkjk <br/>
+            ksdjflkjsdlkfjsldkfjlskdjfklsjdflkjsdlkj <br/>
+        </p>
         <Footer />
         </div>
     )
