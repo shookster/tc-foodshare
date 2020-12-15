60bf7c0e6f3335a0abc2c8ff5dd8b22619586394
will collins <willdoinc@gmail.com>
2020-12-14 20:32:22 -0600

    new

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LoginButton from './LoginButton';
+import LogoutButton from './LogoutButton';
 
 function Navbar() {
     return(
         <div>
             <nav className="navbar navbar-light bg-light">
             <Link className="navbar-brand" to="/React-Portfolio/home">TC - Food Share</Link>
-             <ul className="navbar-nav">
+                <ul className="navbar-nav">
+                     
+                         <LoginButton />
+                            <LogoutButton />
+                      
                  <Link className="nav-item active" to="">Search</Link>
                  <Link className="nav-item active" to="">Login/Signup</Link>
              </ul>
