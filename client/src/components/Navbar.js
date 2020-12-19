3db1e0c35757c783629b9e2eb2501726981d05e2
will collins <willdoinc@gmail.com>
2020-12-19 14:09:24 -0600

    aaaa

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+
 function Navbar() {
-    return(
-        <div>
-            <nav className="navbar navbar-light bg-light">
-            <Link className="navbar-brand" to="/">TC - Food Share</Link>
-                <ul className="navbar-nav">
-                     
-                         <LoginButton />
-                            <LogoutButton />
-                      
-                 <Link className="nav-item active" to="">Search</Link>
-                 <Link className="nav-item active" to="">Login/Signup</Link>
-             </ul>
-             </nav>
-        </div>
-        </nav>
-    )
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <Link className="navbar-brand" to="/">
+          TC - Food Share
+        </Link>
+        <ul className="navbar-nav">
+          <LoginButton />
+          <LogoutButton />
+
+          <Link className="nav-item active" to="">
+            Search
+          </Link>
+          <Link className="nav-item active" to="">
+            Login/Signup
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
 }
 export default Navbar;
 
@@ -32,10 +38,10 @@ export default Navbar;
 //             <nav className="navbar navbar-light bg-light">
 //             <Link className="navbar-brand" to="/React-Portfolio/home">TC - Food Share</Link>
 //                 <ul className="navbar-nav">
-                     
+
 //                          <LoginButton />
 //                             <LogoutButton />
-                      
+
 //                  <Link className="nav-item active" to="">Search</Link>
 //                  <Link className="nav-item active" to="">Login/Signup</Link>
 //              </ul>
